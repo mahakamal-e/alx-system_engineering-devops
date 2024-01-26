bb7eb568f54150c773dd046cb2e341a0b1dc6b59
mahakamal-e <mahakamal09@gmail.com>
2024-01-26 12:46:50 -0800

    zombie task

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+/**
+ * infinite_while - Creates an infinite loop
+ * Return: Always returns 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - the entry point.
+ * Return: always return 0 on sucess.
+ */
+int main(void)
+{
+	pid_t child_pid;
+	char count = 0;
+
+	while (count < 5)
+	{
+		child_pid = fork();
+		if (child_pid < 0)
+		{
+			perror("Fork error");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(EXIT_SUCCESS);
+			count++;
+		}
+	}
+	infinite_while();
+	return (EXIT_SUCCESS);
+}
