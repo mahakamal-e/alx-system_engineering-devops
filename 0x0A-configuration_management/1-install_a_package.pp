d36355bd4872797d68e033c8fbdc958ddaa37f23
mahakamal-e <mahakamal09@gmail.com>
2024-03-22 12:28:06 -0700

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Install Flask
+class { 'python3': }  # Include python3 class for python3 support
 
-package { 'flask':
-  ensure   => '2.1.0',
+package { 'Flask':
+  # Ensure Flask is installed
+  ensure => present,
+  # Use pip3 provider to install from the Python Package Index (PyPI)
   provider => 'pip3',
+  # Specify the required version
+  require => Class['python3'],  # Make sure python3 is installed first
+  # Specify exact version of Flask
+  ensure_version => '2.1.0',
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Task using Puppet, kill prosses using exec
+
+exec{'run a command':
+        command => 'killall -9 killmenow',
+        path    => ['/usr/bin','/usr/sbin','/bin'],
+}
