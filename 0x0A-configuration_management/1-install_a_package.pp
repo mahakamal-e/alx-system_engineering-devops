6a1b69cfe28b53738fbd2bbc2bf4c8b9004d9d43
mahakamal-e <mahakamal09@gmail.com>
2024-03-22 13:06:33 -0700

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
-# install flask 2.1.0 from pip3
+# Install flask from pip3
 package{'flask':
     ensure   => '2.1.0',
-    provider => 'pip3',}
+    provider => 'pip3',
+}
+
+notify { 'Flask installed successfully':
+  message => 'Flask version 2.1.0 installed successfully',
+}
