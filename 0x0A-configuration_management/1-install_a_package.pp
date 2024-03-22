206edced9e1d1b45dcf8ca0ab09c77c01fcb662d
mahakamal-e <mahakamal09@gmail.com>
2024-03-22 12:31:35 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,4 @@
-class { 'python3': }  # Include python3 class for python3 support
-
-package { 'Flask':
-  # Ensure Flask is installed
-  ensure => present,
-  # Use pip3 provider to install from the Python Package Index (PyPI)
-  provider => 'pip3',
-  # Specify the required version
-  require => Class['python3'],  # Make sure python3 is installed first
-  # Specify exact version of Flask
-  ensure_version => '2.1.0',
-}
+# install flask 2.1.0 from pip3
+package{'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',}
