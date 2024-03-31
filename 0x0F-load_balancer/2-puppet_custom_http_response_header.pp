a6cafe7b09dfb0e01fc657b54f7cd66a979780ac
mahakamal-e <mahakamal09@gmail.com>
2024-03-31 11:53:01 -0700

    edit

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,17 +3,37 @@ exec { 'update':
   command  => 'sudo apt-get update',
   provider => shell,
 }
--> package {'nginx':
-  ensure => present,
+package { 'nginx':
+  ensure => latest,
 }
--> file_line { 'header line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
+
+package { 'nginx':
+  ensure => installed,
 }
--> exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+
+firewall { 'Allow Nginx HTTP':
+  ensure  => 'present',
+  dport   => 80,
+  proto   => 'tcp',
+  action  => 'accept',
+}
+
+file { '/etc/nginx/conf.d/custom-header.conf':
+  ensure  => file,
+  content => 'server_tokens off;
+              add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
