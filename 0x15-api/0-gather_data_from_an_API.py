b99139743422e48381e84375314c827f06fa5df9
mahakamal-e <mahakamal09@gmail.com>
2024-04-22 16:26:12 -0700

    task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     employee_name = requests.get(f"{url}users/{employee_id}").json()
-    employee_todo = requests.get(f"{url}?userId={employee_id}").json()
+    employee_todo = requests.get(f"{url}todos?userId={employee_id}").json()
 
     completed_tasks = 0
 
-    for task in emplyee_todo:
-        if employee_todo['completed']:
-            completed_taskes += 1
+    for task in employee_todo:
+        if task['completed']:
+            completed_tasks += 1
 
-    print(f"Employee {employee_name['name']} is done with tasks " \
+    print(f"Employee {employee_name['name']} is done with tasks "
           f"({completed_tasks}/{len(employee_todo)}):")
     for task in employee_todo:
         if task['completed']:
