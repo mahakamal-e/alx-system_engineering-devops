6d756f87bb0c54fe5f29573649b8fb918df2927d
mahakamal-e <mahakamal09@gmail.com>
2024-04-22 17:33:47 -0700

    s

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Using REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+and exports it to a json dict.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+
+    employee_name = requests.get("{}users/{}".format(url, employee_id)).json()
+    employee_todos = requests.get(
+            "{}todos?userId={}".format(url, employee_id)
+    ).json()
+
+    json_data = []
+
+    for todo in employee_todos:
+        json_data.append({
+            "USER_ID": employee_id,
+            "USERNAME": employee_name.get("username"),
+            "TASK_COMPLETED_STATUS": todo.get("completed"),
+            "TASK_TITLE": todo.get("title")
+        })
+    with open("{}.json".format(employee_id), 'w') as file:
+        json.dump(json_data, file, indent=None)
+
