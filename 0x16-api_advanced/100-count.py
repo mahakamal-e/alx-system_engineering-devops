f5b32cbc007b74800fd02ed6b5b8eef1510f8759
mahakamal-e <mahakamal09@gmail.com>
2024-05-07 18:53:10 -0700

    edit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" count words usong recursive function """
+""" count words using recursive function """
 import requests
 
 
 def count_words(subreddit, word_list):
     """
     A recursive function that queries the Reddit API,
-    parses the title of all hot articles, and prints a sorted count of given keywords.
+    parses the title of all hot articles,
+    and prints a sorted count of given keywords.
     """
     # Initialize counts dictionary
     counts = {}
@@ -15,13 +16,16 @@ def count_words(subreddit, word_list):
     _count_words_recursive(subreddit, word_list, counts)
 
     # Sort counts by count (descending) and keyword (ascending)
-    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0].lower()))
+    sorted_counts = sorted(counts.items(),
+                           key=lambda x: (-x[1], x[0].lower()))
     for keyword, count in sorted_counts:
         print(f"{keyword.lower()}: {count}")
 
+
 def _count_words_recursive(subreddit, word_list, counts, after=None):
     """
-    A recursive helper function to query the Reddit API and count occurrences of keywords.
+    A recursive helper function to query the Reddit API,
+    and count occurrences of keywords.
     """
     # Base case: if word_list is empty, return
     if not word_list:
@@ -54,7 +58,6 @@ def _count_words_recursive(subreddit, word_list, counts, after=None):
 
         # Check if there are more pages of results
         after = data['data']['after']
-        # Recursively call _count_words_recursive with the next keyword and updated counts
         _count_words_recursive(subreddit, word_list[1:], counts, after)
     elif response.status_code == 404:
         # If subreddit is not found, return
