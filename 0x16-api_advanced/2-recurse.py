2efa13102bea7edfb9e79c16383776297f5d27f7
mahakamal-e <mahakamal09@gmail.com>
2024-05-07 18:31:56 -0700

    edit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
-'Gets top 10 most host posts of a subreddit using reddit api.'
+""" implement recurse func """
 import requests
-import time
 
 
 def recurse(subreddit, hot_list=[], params=None):
-    """Gets top 10 most host posts of a subreddit using reddit api."""
+    """
+    queries the Reddit API and returns a list,
+    containing the titles of all hot articles
+    for a given subreddit
+    """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'MyApp'}
 
