b9512fae47955edb173a68d18626921f41540766
mahakamal-e <mahakamal09@gmail.com>
2024-05-07 17:46:35 -0700

    edit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,16 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts
-    listed for a given subreddit"""
-
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    res = requests.get(url, headers={'User-Agent': 'AyaTarek95'},
-                       allow_redirects=False)
-
-    if res.status_code == 200:
-        posts = res.json().get('data', {}).get('children', [])
-        for post in posts:
-            print(post.get('data', {}).get('title', ''))
-    else:
+    """"Queries the Reddit API and prints the first 10 hot posts titles"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'MyApp'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    elif response.status_code == 404:
         print(None)
+    else:
+        print("Error:", response.status_code)
