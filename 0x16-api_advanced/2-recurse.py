4c914490298cc48bb7ecc749c27b53183aecbadc
mahakamal-e <mahakamal09@gmail.com>
2024-05-07 18:07:23 -0700

    edit 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,33 +8,23 @@ def recurse(subreddit, hot_list=None, after=None):
     A recursive function that queries the Reddit API,
     and returns a list containing the titles of all hot articles
     """
-    if hot_list is None:
-        hot_list = []  # Initialize hot_list if not provided
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    params = {'limit': 100, 'after': after}
+    headers = {'User-Agent': 'custom user-agent'}
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    if after:
-        url += f"&after={after}"
-    headers = {'User-Agent': 'MyApp'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url,
+                            params=params,
+                            headers=headers,
+                            allow_redirects=False)
 
-    # Check for successful response
-    if response.status_code != 200:
-        print("Error:", response.status_code)
+    if response.status_code == 404:
         return None
 
     data = response.json().get('data', {})
+    for post in data.get('children', []):
+        hot_list.append(post.get('data').get('title'))
 
-    # Check if 'children' key exists in the data
-    if 'children' in data:
-        # Extract titles of hot posts
-        for post in data['children']:
-            hot_list.append(post['data'].get('title'))
-
-        # Recursively call the function if there are more pages
-        if data.get('after'):
-            return recurse(subreddit, hot_list, data['after'])
-        else:
-            return hot_list
+    if data.get('after'):
+        return recurse(subreddit, hot_list, data.get('after'))
     else:
-        print("Error: Unexpected response format")
-        return None
+        return hot_list
