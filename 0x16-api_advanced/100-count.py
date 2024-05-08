f55bcbbfce5f46c8fb3845437323b231d201fd2c
mahakamal-e <mahakamal09@gmail.com>
2024-05-07 19:15:59 -0700

    addadv

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -23,7 +23,7 @@ def fetch_hot_articles(subreddit, limit=100, after=None, titles=[]):
             titles.append(post['data']['title'].lower())
         after = data['data']['after']
         if after:
-            time.sleep(2)  # Rate limiting to quently
+            time.sleep(2)
             return fetch_hot_articles(subreddit, limit, after, titles)
         else:
             return titles
@@ -36,22 +36,15 @@ def count_words(subreddit, word_list, titles=[], counts={}):
     Returns:
     None
     """
-    # Fetch hot articles from the subreddit
     titles = fetch_hot_articles(subreddit)
-    
+
     if not titles:
-        # If subreddit is invalid or no posts match, print nothing
         return
-    
-    # Count occurrences of each word in the titles
+
     for title in titles:
         for word in word_list:
             if word.lower() in title:
                 counts[word.lower()] = counts.get(word.lower(), 0) + 1
-    
-    # Sort counts dictionary by values (count) and then by keys (alphabetically)
     sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-    
-    # Print the sorted counts
     for word, count in sorted_counts:
         print(f"{word}: {count}")
