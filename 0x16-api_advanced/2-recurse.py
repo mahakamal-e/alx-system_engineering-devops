bb7d3f52e780089f90ac3f3892144f649ae1d50b
mahakamal-e <mahakamal09@gmail.com>
2024-05-07 18:22:04 -0700

    edd

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-""" Using recrsion to get title of top 10 hot posts"""
+
+'Gets top 10 most host posts of a subreddit using reddit api.'
 import requests
+import time
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Return list of titles for all hot posts on 
-    subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    headers = {"User-Agent": "MyApp"}
-    response = requests.get(url, headers=headers,
-                            params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[], params=None):
+    """Gets top 10 most host posts of a subreddit using reddit api."""
+
+    headers = {'User-Agent': 'alxAPI'}
+
+    res = requests.get(
+        'https://www.reddit.com/r/{}/hot.json'.format(subreddit),
+        headers=headers,
+        allow_redirects=False,
+        params=params
+    )
+
+    if res.status_code == 200:
+        posts = res.json().get('data').get('children')
+        hot_list.extend(posts)
+        after = res.json().get('data').get('after')
+        if after:
+            params = {'after': after}
+            return recurse(subreddit, hot_list, params)
+        else:
+            return hot_list
+    elif res.status_code == 429:
+        time.sleep(1)
+        return recurse(subreddit, hot_list, params)
+    else:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
