77b8a6f147f406961c247e598a135affec28742d
mahakamal-e <mahakamal09@gmail.com>
2024-05-16 04:59:53 -0700

    add task1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,18 +1,12 @@
 # Change the OS configuration so that,
 # it is possible to login with the holberton user open a file.
 exec { 'update_soft_limit':
-  command => "sed -i '/^holberton\\s.*nofile/d; $ a\\holberton soft nofile 4096' /etc/security/limits.d/holberton.conf",
-  path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-  notify  => Exec['reload_pam'],
+  command => 'sed -i "/holberton soft/s/4/4096/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'update_hard_limit':
-  command => "sed -i '/^holberton\\s.*nofile/d; $ a\\holberton hard nofile 8192' /etc/security/limits.d/holberton.conf",
-  path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-  notify  => Exec['reload_pam'],
+  command => 'sed -i "/holberton hard/s/5/4096/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'reload_pam':
-  command     => 'pam-auth-update --force',
-  refreshonly => true,
-}
